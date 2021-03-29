b79ac260fd8d2227bfab489f2893638ac724cc52
Dave Hearne <dhearne@tssg.org>
2021-03-29 08:05:43 +0100

    class exercises

--- app/src/main/java/ie/wit/placemark/main/MainApp.kt
+++ app/src/main/java/ie/wit/placemark/main/MainApp.kt
@@ -1,17 +1,18 @@
 package ie.wit.placemark.main
 
 import android.app.Application
-import ie.wit.placemark.models.PlacemarkMemStore
-import ie.wit.placemark.models.PlacemarkModel
+import ie.wit.placemark.models.PlacemarkJSONStore
+import ie.wit.placemark.models.PlacemarkStore
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 
 class MainApp : Application(), AnkoLogger {
 
-    val placemarks = PlacemarkMemStore()
+    lateinit var placemarks: PlacemarkStore
 
     override fun onCreate() {
         super.onCreate()
+        placemarks = PlacemarkJSONStore(applicationContext)
         info("Start App From Here!")
 
 //        placemarks.add(PlacemarkModel("One", "About one..."))
